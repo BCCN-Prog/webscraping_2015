f21abea26c80d589ce5be805d6c31a8b420d3442
Konotori <mark.neznansky@posteo.de>
2015-05-04 10:22:08 +0200

    Python 3 compatible city-name->index function
    (>")>

--- weathercom_get_city_index.py
+++ weathercom_get_city_index.py
@@ -1,17 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Apr 24 15:31:34 2015
-
-@author: Mark
+Takes a city name string, and returns a string containing the index used on 
+weather.com
 """
 
-# Written for python 2. Supposedly one can uncommet the lines that contain
-# the <<<<<<<<<<<< marking and delete one line above these to make it 
-# work with python 3.
 
 
-import urllib2      # Python 2 
-# import urllib2  # <<<<<<<<<<<<<<<<<<
+import urllib
 
 def weathercom_get_city_index(city): 
     '''
@@ -23,8 +18,8 @@ def weathercom_get_city_index(city):
     + '.js?locType=1&cb=twctacb_575661617_4&' \
     + 'key=2227ef4c-dfa4-11e0-80d5-0022198344f4&max=20&locale=en_US'
     
-    page = urllib2.urlopen(url)
-#    page = urllib.request.urlopen(url) #<<<<<<<<<<
+
+    page = urllib.request.urlopen(url) 
     index = page.readlines()[1]
-    index = index[19:-2]
-    return index
+    index = index[19:-3]
+    return index.decode(encoding='UTF-8')
