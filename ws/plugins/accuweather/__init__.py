577da237d219aacd4413cb789fb08c76ca218223
kolafix <kolafix@users.noreply.github.com>
2015-05-15 15:18:56 +0100

    Use os.path.join() to join paths

--- ws/plugins/accuweather/__init__.py
+++ ws/plugins/accuweather/__init__.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import pickle
 import os
@@ -6,23 +5,17 @@ import sys
 import ws.bad as bad
 
 mydir = os.path.abspath(os.path.dirname(__file__))
-
-print(mydir)
-
-lookupmatrix = pickle.load(open( \
-                               mydir +'/accuweather_location_codes.dump','rb'))
-
+lookupmatrix = pickle.load(open(os.path.join(mydir, 'accuweather_location_codes.dump'), 'rb'))
 lookuplist = lookupmatrix.tolist()
 
 def build_url(city):
     # check whether input is a string
     if type(city) != str:
         raise(bad.Type("The input city " +str(city) +" wasn't of type string"))
-        
-    
+
     index = lookuplist[1].index(city)
     accuweather_index = lookuplist[0][index]
-    
+
     url = 'http://realtek.accu-weather.com/widget/realtek/weather-data.asp' \
           + '?location=cityId:' \
           + str(accuweather_index)
