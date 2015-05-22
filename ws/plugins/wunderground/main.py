3cf4e0657929ec89a2daceb2d50caa8fa076282a
Konotori <mark.neznansky@posteo.de>
2015-05-22 17:12:13 +0200

    Clocked wunderground query
    Added extra keys; a function that times and limits the query to 10 * 9
    requests per limit. (a per-day limit was not implemented under the
    assumption it would not be reached (9*500 queries / day).

--- ws/plugins/wunderground/main.py
+++ ws/plugins/wunderground/main.py
@@ -5,7 +5,9 @@
 
 import urllib
 import json
+import time
 import pandas as pd
+import numpy as np
 #import bad
 
 def build_url(city):
@@ -13,8 +15,8 @@ def build_url(city):
     input must be a string, containing the city name alone
     '''
     
-    url = 'http://api.wunderground.com/api/3a8e74a2827886a1/geolookup/'\
-    'conditions/q/DL/'+city+'.json'
+    url = 'http://api.wunderground.com/api/'+return_wundergroud_key+\
+    '/geolookup/conditions/q/DL/'+city+'.json'
     page = urllib.request.urlopen(url) 
     read = page.read()
     decoded = read.decode('utf8')
@@ -30,10 +32,10 @@ def build_url(city):
         except:
             raise bad.city()
             
-    forecasturl = 'http://api.wunderground.com/api/3a8e74a2827886a1/forecast10day'\
-    +cityname+'.json'
+    forecasturl = 'http://api.wunderground.com/api/'+return_wundergroud_key+\
+    '/forecast10day'+cityname+'.json'
     return forecasturl
-    
+
 def pandize(url,cityname):
     page = urllib.request.urlopen(url) 
     read = page.read()
@@ -66,3 +68,62 @@ str(data["forecast"]["simpleforecast"]["forecastday"][0]['date']['year'])\
 ,forecast["snow_day"]["cm"]\
 ,forecast["snow_night"]["cm"]]
     return table
+
+###############################
+################################
+
+clocktesterarray = np.zeros(500)
+def clocktester(n):
+    ''' manual testing tool for clocker() function.'''
+    for i in range(n):
+        clocktesterarray[i] = time.time()
+        clocker()
+        
+def clocker():
+    '''Let the clocked function work at full speed while possibl, then wait
+    until the period ends that allows it to go again.'''
+
+    global wunderground_keys_time_ticks_
+    if 'wunderground_keys_time_ticks_' not in globals():
+        wunderground_keys_time_ticks_ = np.zeros(90)
+    if wunderground_keys_time_ticks_[-1] == 0:
+        wunderground_keys_time_ticks_ = np.roll(wunderground_keys_time_ticks_, 1)
+        wunderground_keys_time_ticks_[0] = time.time()
+    else:
+        deltatee = wunderground_keys_time_ticks_[0] - wunderground_keys_time_ticks_[-1]
+        if deltatee < 61:
+            time.sleep(61-deltatee)
+            wunderground_keys_time_ticks_ = np.roll(wunderground_keys_time_ticks_, 1)
+            wunderground_keys_time_ticks_[0] = time.time()
+        else:
+            wunderground_keys_time_ticks_ = np.roll(wunderground_keys_time_ticks_, 1)
+            wunderground_keys_time_ticks_[0] = time.time()
+
+
+def return_wundergroud_key():
+    global wunderground_keys_
+
+    key_well_ = [\
+    '3a8e74a2827886a1',\
+    '8714d2a1dfc6e7a8',\
+    '23e7a53f72f058ac',\
+    '7b88114d03edd183',\
+    '6392e7cab4ead3bf',\
+    'e709118692e74123',\
+    '15858ca917d7dfae',\
+    'a930fdbbb56c3b59',\
+    '3ef23167f819b269'\
+    ]
+    
+    try:
+        if len(wunderground_keys_) > 0:
+            clocker()
+            return wunderground_keys_.pop()
+        else:
+            wunderground_keys_ = key_well_.copy()
+            clocker()
+            return wunderground_keys_.pop()
+    except:
+        wunderground_keys_ = key_well_.copy()
+        clocker()
+        return wunderground_keys_.pop()        
