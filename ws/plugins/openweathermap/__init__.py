2c81a6a5c0040ba0e335f4b0afafe261a961e422
Malte <malte.esders@gmail.com>
2015-05-22 16:08:55 +0200

    added openweathermap plugin

---
+++ ws/plugins/openweathermap/__init__.py
@@ -0,0 +1,14 @@
+import ws.bad as bad
+
+def build_url(city):
+    # check whether input is a string
+    if type(city) != str:
+        raise(bad.Type("The input city " +str(city) +" wasn't of type string"))
+
+    url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' \
+          + city \
+          + '&mode=xml' \
+          + '&units=metric' \
+          + '&cnt=16' # 'cnt = 16' means next 16 days
+
+    return url
