29864b2b935b8f9430df88808d82e71ca55cffef
Konotori <mark.neznansky@posteo.de>
2015-05-29 15:43:48 +0200

    Weather.com build_url function *crucial update*
    fixed

--- ws/plugins/weatherdotcom/main.py
+++ ws/plugins/weatherdotcom/main.py
@@ -3,7 +3,7 @@
 ##### This is weather.com ###
 
 import urllib
-import ws.bad as bad
+# import ws.bad as bad
 import json
 
 def weathercom_get_city_index(city): 
@@ -26,26 +26,11 @@ def build_url(city):
     '''
     input must be a string, containing the city name alone
     '''
+    city_index = weathercom_get_city_index(city)
+    url = 'http://dsx.weather.com/%28wxd/v2/MORecord;wxd/v2/wwir;wxd/'\
+    +'v2/PastFlu;wxd/v2/SkiResorts;wxd/v2/DIRecord;wxd/v2/DFRecord;wxd/'\
+    +'v2/DHRecord;wxd/v2/BERecord;wxd/v2/Pollen;wxd/v2/airportDelays;wxd/v2/'\
+    +'TIRecord;wxd/v2/WMRecord%29/%28en_US/'+city_index\
+    +':1:GM%29?api=01119904-40b2-4f81-94a0-57867d0fd22c'
+    return url
     
-    url = 'http://api.wunderground.com/api/3a8e74a2827886a1/geolookup/'\
-    'conditions/q/DL/'+\
-    weathercom_get_city_index(city)\
-    +'.json'
-    page = urllib.request.urlopen(url) 
-    read = page.read()
-    decoded = read.decode('utf8')
-    data = json.loads(decoded)
-    
-    if "location" in data.keys():     # "location" case
-        cityname =  data["location"]["l"]
-    else:                               # "Berlin" case (ambiguity)
-        try:
-            for cities in data["response"]["results"]:
-                if cities["country"] == "DL":
-                    cityname = cities["l"]
-        except:
-            raise bad.City()
-            
-    forecasturl = 'http://api.wunderground.com/api/3a8e74a2827886a1/forecast10day'\
-    +cityname+'.json'
-    return forecasturl
