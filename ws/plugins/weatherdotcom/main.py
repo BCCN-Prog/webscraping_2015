23c864affce2efd005572bb39d216feed4630dd8
kolafix <kolafix@users.noreply.github.com>
2015-05-29 15:08:22 +0100

    Parse the city index for weather.com from the json
    write tests for get_city_index()
    
    change build_url() to the correct function.

--- ws/plugins/weatherdotcom/main.py
+++ ws/plugins/weatherdotcom/main.py
@@ -2,41 +2,57 @@ import urllib.request
 import ws.bad as bad
 import json
 import logging
+import re
 
-def weathercom_get_city_index(city):
+
+def get_city_index(city):
     '''
     input must be a string, containing the city name alone
     '''
 
-    url = 'http://wxdata.weather.com/wxdata/ta/' \
-    + city + ',germany' \
-    + '.js?locType=1&cb=twctacb_575661617_4&' \
-    + 'key=2227ef4c-dfa4-11e0-80d5-0022198344f4&max=20&locale=en_US'
+    url = ('http://wxdata.weather.com/wxdata/ta/' + city + ',germany' +
+           '.js?locType=1&cb=twctacb_575661617_4&' +
+           'key=2227ef4c-dfa4-11e0-80d5-0022198344f4&max=20&locale=en_US')
+
+    logging.debug('city %s url %s', city, url)
 
     with urllib.request.urlopen(url) as response:
         data = response.read()
         data = data.decode(encoding='UTF-8')
-        print(data)
 
-    page = urllib.request.urlopen(url)
-    index = page.readlines()[1]
-    index = index[19:-3]
-    index = index.decode(encoding='UTF-8')
+        # Prune the data to a pure json string
+        data = re.sub('^[a-z0-9_]+\(', '', data)
+        data = re.sub('\)$', '', data)
 
-    logging.debug('city %s index %s url %s', city, index, url)
+        index_dict = json.loads(data)
+
+        # Select the first match for this city. If there is no match, raise a
+        # bad.City exception.
+        num_results = len(index_dict['results'])
+        if num_results > 0:
+            first_match = index_dict['results'][0]
+            index = first_match['key']
+        else:
+            raise bad.City
+
+    logging.debug('index %s', index)
 
     return index
+
 
 def build_url(city):
     '''
     input must be a string, containing the city name alone
     '''
 
-    index = weathercom_get_city_index(city)
-    url = 'http://dsx.weather.com/%28wxd/v2/MORecord;wxd/v2/wwir;wxd/v2/'\
-            +'PastFlu;wxd/v2/SkiResorts;wxd/v2/DIRecord;wxd/v2/DFRecord;wxd/v2/'\
-            +'DHRecord;wxd/v2/BERecord;wxd/v2/Pollen;wxd/v2/airportDelays;wxd/v2/'\
-            +'TIRecord;wxd/v2/WMRecord%29/%28en_US/'\
-            + index +':1:GM%29?api=01119904-40b2-4f81-94a0-57867d0fd22c'
+    index = get_city_index(city)
+    url = ('http://dsx.weather.com/'
+           + '%28wxd/v2/MORecord;wxd/v2/wwir;wxd/v2/'
+           + 'PastFlu;wxd/v2/SkiResorts;wxd/v2/DIRecord;wxd/v2/'
+           + 'DFRecord;wxd/v2/'
+           + 'DHRecord;wxd/v2/BERecord;wxd/v2/Pollen;wxd/v2/'
+           + 'airportDelays;wxd/v2/'
+           + 'TIRecord;wxd/v2/WMRecord%29/%28en_US/' + index
+           + ':1:GM%29?api=01119904-40b2-4f81-94a0-57867d0fd22c')
 
     return url
--- ws/plugins/weatherdotcom/tests/test_get_city_index.py
+++ ws/plugins/weatherdotcom/tests/test_get_city_index.py
@@ -1,16 +1,16 @@
 import unittest
 import ws.bad as bad
 
-from ..main import weathercom_get_city_index
+from ..main import get_city_index
 
 class get_city_indexTest(unittest.TestCase):
 
     def test_ahrensfelde(self):
-        self.assertEqual(weathercom_get_city_index('ahrensfelde'), 'GMXX7358')
+        self.assertEqual(get_city_index('ahrensfelde'), 'GMXX7358')
 
     def test_invalid_city(self):
         with self.assertRaises(bad.City):
-            weathercom_get_city_index('GermanyHasManyFunCityNamesButNotThisOne')
+            get_city_index('GermanyHasManyFunnyCityNamesButNotThisOne')
 
 
 if __name__ == '__main__':
