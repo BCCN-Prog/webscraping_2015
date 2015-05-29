b3d612e0dd33187472307ac08c308d424f44a6d0
Konotori <mark.neznansky@posteo.de>
2015-05-29 17:37:06 +0200

    Pandize - .com
    new and improved

--- ws/plugins/weatherdotcom/main.py
+++ ws/plugins/weatherdotcom/main.py
@@ -44,15 +44,49 @@ def build_url(city):
     '''
     input must be a string, containing the city name alone
     '''
-
-    index = get_city_index(city)
-    url = ('http://dsx.weather.com/'
-           + '%28wxd/v2/MORecord;wxd/v2/wwir;wxd/v2/'
-           + 'PastFlu;wxd/v2/SkiResorts;wxd/v2/DIRecord;wxd/v2/'
-           + 'DFRecord;wxd/v2/'
-           + 'DHRecord;wxd/v2/BERecord;wxd/v2/Pollen;wxd/v2/'
-           + 'airportDelays;wxd/v2/'
-           + 'TIRecord;wxd/v2/WMRecord%29/%28en_US/' + index
-           + ':1:GM%29?api=01119904-40b2-4f81-94a0-57867d0fd22c')
-
+    city_index = get_city_index(city)
+    url = 'http://dsx.weather.com/%28wxd/v2/MORecord;wxd/v2/wwir;wxd/'\
+    +'v2/PastFlu;wxd/v2/DIRecord;wxd/'\
+    +'v2/DHRecord;wxd/v2/BERecord;wxd/v2/'\
+    +'TIRecord;wxd/v2/WMRecord%29/%28en_US/'+city_index\
+    +':1:GM%29?api=01119904-40b2-4f81-94a0-57867d0fd22c'
     return url
+    
+# DI Record is what we want (forecast for a week or so)
+    
+def pandize(url, cityname, date):
+    page = urllib.request.urlopen(url)
+    read = page.read()
+    decoded = read.decode('utf8')
+    data = json.loads(decoded)
+    table = pd.DataFrame(columns = ['ref_date','city','pred_offset','Min Air Temp','Max Air Temp','avg_wind_speed',\
+    'avg_wind_direction','max_wind_speed','max_wind_direction','avg_humidity'\
+    ,'percp_total','percp_day','percp_night','snow_total','snow_day','snow_night'])
+    return data
+'''
+    for i in range(9):
+        forecast = data["forecast"]["simpleforecast"]["forecastday"][i]
+        table.loc[i] = [\
+pd.to_datetime([\
+str(data["forecast"]["simpleforecast"]["forecastday"][0]['date']['day'])+'.'+\
+str(data["forecast"]["simpleforecast"]["forecastday"][0]['date']['month'])+'.'+\
+str(data["forecast"]["simpleforecast"]["forecastday"][0]['date']['year'])\
+], dayfirst=True)\
+,cityname\
+,int(i)\
+,forecast["low"]['celsius']\
+,forecast["high"]['celsius']\
+,forecast["avewind"]['kph']
+,forecast["avewind"]['degrees']\
+,forecast["maxwind"]['kph']\
+,forecast["maxwind"]['degrees']\
+,forecast["avehumidity"]\
+,forecast["qpf_allday"]["mm"]\
+,forecast["qpf_day"]["mm"]\
+,forecast["qpf_night"]["mm"]\
+,forecast["snow_allday"]["cm"]\
+,forecast["snow_day"]["cm"]\
+,forecast["snow_night"]["cm"]]
+    return table
+
+'''
